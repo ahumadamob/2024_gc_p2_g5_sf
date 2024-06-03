559f3a74dc87a5beebcf9fe798296398adf49e5a
PHFM80 <pablofloresmaza@gmail.com>
2024-06-03 11:46:36 -0300

    dos commit eliminados, entity metodo de pago creada si persisitir

---
+++ src/main/java/gc/_4/pr2/grupo5/entity/Envios.java
@@ -0,0 +1,62 @@
+package gc._4.pr2.grupo5.entity;
+
+import java.util.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+
+@Entity
+@Table(name = "envios")
+public class Envios {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String direccionEnvio;
+	private int fechaEnvio;
+	private String empresaTrasnporte;
+	private int numeroSeguimiento;
+	
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getDireccionEnvio() {
+		return direccionEnvio;
+	}
+	public void setDireccionEnvio(String direccionEnvio) {
+		this.direccionEnvio = direccionEnvio;
+	}
+	
+	public int getFechaEnvio() {
+		return fechaEnvio;
+	}
+	public void setFechaEnvio(int fechaEnvio) {
+		this.fechaEnvio = fechaEnvio;
+	}
+	public String getEmpresaTrasnporte() {
+		return empresaTrasnporte;
+	}
+	public void setEmpresaTrasnporte(String empresaTrasnporte) {
+		this.empresaTrasnporte = empresaTrasnporte;
+	}
+	public int getNumeroSeguimiento() {
+		return numeroSeguimiento;
+	}
+	public void setNumeroSeguimiento(int numeroSeguimiento) {
+		this.numeroSeguimiento = numeroSeguimiento;
+	}
+	
+	
+	
+	
+
+
+}
