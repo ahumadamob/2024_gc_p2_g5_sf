46aeb8f9e6514d12d69f353adb6f455c46773763
Mimila24 <wnyamilagomez@gmail.com>
2024-09-05 20:04:20 -0300

    Revert "persistencia simple entidad CarritoCompras"
    This reverts commit f5fb47dec03eff33b98ce46e301ffc108e55cef1.

--- src/main/java/gc/_4/pr2/grupo5/repo/CarritoComprasRepository.java
+++
@@ -1,9 +0,0 @@
-package gc._4.pr2.grupo5.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import gc._4.pr2.grupo5.entity.CarritoCompras;
-public interface CarritoComprasRepository extends JpaRepository<CarritoCompras, Long>{
-	
-
-}
