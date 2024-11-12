b37b5bb9749ac41b6d51c77c9caf48b20575e698
ohagusho <agusparedez66@gmail.com>
2024-11-12 19:02:55 -0300

    persistencia simple de la entidad envios realiazada

--- src/main/java/gc/_4/pr2/grupo5/service/impl/EnviosServiceImpl.java
+++ src/main/java/gc/_4/pr2/grupo5/service/impl/EnviosServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class EnviosServiceImpl implements EnviosService {
@@ -26,11 +27,17 @@ public class EnviosServiceImpl implements EnviosService {
 
     @Override
     public Envios createOrUpdateEnvio(Envios envio) {
-        return enviosRepository.save(envio);
+        // Ejemplo de instanciación e inicialización: guardamos (inicializamos) un objeto 'Envios' en la base de datos
+        return enviosRepository.save(envio); // El objeto 'envio' es instanciado y se inicializa al ser guardado
     }
 
     @Override
     public void deleteEnvio(Long id) {
         enviosRepository.deleteById(id);
+    }
+
+    @Override
+    public boolean exists(Long id) {
+        return enviosRepository.existsById(id);
     }
 }
